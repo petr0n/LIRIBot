5e245a01df69041a81605158d94c5387eb8781b4
petr0n <petron@gmail.com>
2019-04-01 18:08:46 -0500

    small updates

--- index.js
+++ index.js
@@ -9,62 +9,74 @@ const log = console.log;
 // keys
 const keys = require('./keys.js');
 
-
-let action = '';
-let name = [];
-let args = process.argv;
-args.map((item,index) => {
-    if (index == 2) { 
-        action = item;
-    } else if (index > 2){
-        name.push(item);
-    }
-});
-
-switch(action) {
-    case 'spotify-this-song':
-        spotifyThisSong(name.join(' '));
-        break;
-    case 'concert-this':
-        concertThis(name.join(' '));
-        break;
-    case 'movie-this':
-        movieThis(name.join(' '));
-        break;
-    default:
-        showHelp();
+if (process.argv) {
+	let args = process.argv;
+	let name = args.slice(3, args.length);
+	// console.log(name);
+	start(process.argv[2], name.join(' '));
 }
 
+
+function start(action, name){
+	switch(action) {
+		case 'spotify-this-song':
+			spotifyThisSong(name);
+			break;
+		case 'concert-this':
+			concertThis(name);
+			break;
+		case 'movie-this':
+			movieThis(name);
+			break;
+		case 'do-what-it-says':
+			let line = getRandomLine();
+			line = line.split(' ');
+			name = line.slice(2, line.length);
+			start(line[0], name.join(' '));
+			// console.log(name.join(' '));
+			break;
+		default:
+			showHelp();
+	}
+
+	// write to log 
+	let now = new Date();
+	logger(moment(now).format('YYYY-MM-DD h:mm:ssA') + ' - ACTION: ' + action + ' QUERY: ' + name);
+
+}
 
 
 ////////////////////////////////////////////////////////
 //                      SPOTIFY                       //
 ////////////////////////////////////////////////////////
 
- 
 function spotifyThisSong(name) {
-    let sQuery = name.length > 0 ? name : 'The Sign';
-
-    if (sQuery.length > 0) {
-        let spotify = new Spotify({
-            id: keys.key.spotify_key,
-            secret: keys.key.spotify_secret
-        });
-        spotify.search({ type: 'track', query: sQuery }, function(err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
-            
-            /*
-            Artist(s)
-            The song's name
-            A preview link of the song from Spotify
-            The album that the song is from
-            */
-           
-           console.log(data); 
-        });
-    }
+	let sQuery = name.length > 0 ? name : 'The Sign';
+	if (sQuery.length > 0) {
+		let spotify = new Spotify({
+			id: keys.key.spotify_key,
+			secret: keys.key.spotify_secret
+		});
+		spotify.search({ type: 'track', query: sQuery }, function(err, data) {
+			if (err) {
+				return console.log('Error occurred: ' + err);
+			}
+		   //console.log(data.tracks.items);
+		   let items = data.tracks.items;
+		   if (items.length > 0) {
+				details = `Song: ${sQuery}
+					Artist: ${data.tracks.items[0].album.artists[0].name} 
+					Album: ${data.tracks.items[0].album.name}
+					Preview URL: ${data.tracks.items[0].preview_url}`;
+			   
+				console.log(buildMessage('success', details));                
+				logger('RESULT: success');                
+			} else {
+				console.log(buildMessage('success', 'No matching tracks found'));
+				logger('RESULT: fail');
+			}
+		});
+	}
 }
 
 
@@ -74,43 +86,37 @@ function spotifyThisSong(name) {
 ////////////////////////////////////////////////////////
 
 function concertThis(name) {
+	let bQuery = name.length > 0 ? name : 'Massive Attack';
+	let bandsInTownApiUrl = 'https://rest.bandsintown.com/artists/' + encodeURI(bQuery) + '/events?&date=upcoming&app_id=' + keys.key.bit_key;
+	let details = '';
+	if (bQuery.length > 0){
+		// console.log(bandsInTownApiUrl);
+		axios.get(bandsInTownApiUrl)
+		.then(function(res){
+			// console.log(res.data);
+			let concerts = res.data;
+			if (concerts.length > 0) {
+				
+				totalConcerts = concerts.length < 3 ? concerts.length : 3;
+				// console.log(chalk.magenta.bgYellowBright.bold('\n Next three concert dates for ' + bQuery + ' '));
+				for (var i = 0; i < totalConcerts;i++) {
+					concertDate = moment(concerts[i].datetime).format('MM/DD/YYYY');
+					details += `Venue: ${concerts[i].venue.name}
+						Location: ${concerts[i].venue.city}
+						Date: ${concertDate}
+						-----------------------\n`;
+				}
 
-    let bQuery = name.length > 0 ? name : 'Massive Attack';
-    let bandsInTownApiUrl = 'https://rest.bandsintown.com/artists/' + encodeURI(bQuery) + '/events?&date=upcoming&app_id=' + keys.key.bit_key;
-    let details = '';
-    if (bQuery.length > 0){
-        // console.log(bandsInTownApiUrl);
-        axios.get(bandsInTownApiUrl)
-        .then(function(res){
-            console.log(res.data);
-            let concerts = res.data;
-            if (concerts.length > 0) {
-                console.log(chalk.magenta.bgYellowBright.bold('\n Next three concert dates for ' + bQuery + ' '));
-                details = '=====================\n';
-                for (var i = 0; i < 3;i++) {
-                    concertDate = moment(concerts[i].datetime).format('MM/DD/YYYY');
-                    details += `
-    Venue: ${concerts[i].venue.name}
-    Location: ${concerts[i].venue.city}
-    Date: ${concertDate}
+				logger('RESULT: success');
+				console.log(buildMessage('success', details));
+			} else {
+				details = `Sorry, no upcoming concerts for ${bQuery} were found.\n`;
 
-=====================
-                    `;
-                }
-                console.log(chalk.yellow(details));
-            } else {
-                details = `
-=====================
-
-    Sorry, no upcoming concerts for ${bQuery} were found.
-
-=====================\n
-`;
-                console.log(chalk.red(details));
-
-            }
-        });
-    }
+				logger('RESULT: fail');
+				console.log(buildMessage('fail', details));
+			}
+		});
+	}
 }
 
 
@@ -122,68 +128,85 @@ function concertThis(name) {
 ////////////////////////////////////////////////////////
 
 function movieThis(name) {
+	let movieTitle = name.length > 0 ? name : 'Mr. Nobody';
+	if (movieTitle.length > 0){
+		let omdbApiUrl = 'https://www.omdbapi.com/?apikey=' + keys.key.omdb_key + '&t=' + encodeURI(movieTitle);
+		// console.log(omdbApiUrl);
 
-    let movieTitle = name.length > 0 ? name : 'Mr. Nobody';
-    if (movieTitle.length > 0){
-        let omdbApiUrl = 'https://www.omdbapi.com/?apikey=' + keys.key.omdb_key + '&t=' + encodeURI(movieTitle);
-        //console.log(omdbApiUrl);
+		axios.get(omdbApiUrl)
+			.then(function(res){
+				// console.log(res.data);
+				if (res.data.Response == 'False') {
+					details = `${movieTitle}\n${res.data.Error}`;
 
-        axios.get(omdbApiUrl)
-            .then(function(res){
-                //console.log(res.data);
-                let movie = res.data;
-                let details = '';
-                if (movie.Title.length > 0) {
-                    details = `
-=====================
+					console.log(buildMessage('fail', details));
+					logger('RESULT: fail');
+				} else {
+					let movie = res.data;
+					let rottenToms = '';
+					if (movie.Ratings.length > 1) {
+						rottenToms = '\nRotten Tomatoes Rating: ' + movie.Ratings[1].Value;
+					}
+					try {
+						let details = `${chalk.bold.green(movie.Title)}
+								Release year: ${movie.Year}
+								IMDB Rating: ${movie.Ratings[0].Value}${rottenToms}
+								Origin: ${movie.Country}
+								Language: ${movie.Language}
+								Actors: ${movie.Actors}
+								Plot: ${movie.Plot}`;
+						console.log(buildMessage('success', details));
+						logger('RESULT: success');
+					}
+					catch(err) {
+						console.log(err.message);
+					}
+				}
+			});
+	}
+}
 
-    ${chalk.bold.green(movie.Title)}
-    Release year: ${movie.Year}
-    IMDB Rating: ${movie.Ratings[0].Value}
-    Rotten Tomatoes Rating: ${movie.Ratings[1].Value}
-    Origin: ${movie.Country}
-    Language: ${movie.Language}
-    Actors: ${movie.Actors}
-    Plot: ${movie.Plot}
-
-=====================
-                `;
-                    console.log(chalk.yellow(details));
-                } else {
-                    details = `
-=====================
-
-    Sorry, nothing found for ${movieTitle}
-
-    =====================
-                    `;
-                    console.log(chalk.red(details));
-                }      
-    /*
-    * Title of the movie.
-    * Year the movie came out.
-    * IMDB Rating of the movie.
-    * Rotten Tomatoes Rating of the movie.
-    * Country where the movie was produced.
-    * Language of the movie.
-    * Plot of the movie.
-    * Actors in the movie.
-    */
-            });
-    }
+function getRandomLine(){
+	let data = fs.readFileSync('random.txt', 'utf8');
+	var lines = data.split('\n');
+	// console.log(lines[Math.floor(Math.random()*lines.length)]);
+	return lines[Math.floor(Math.random()*lines.length)]
 }
 
 function showHelp() {
-    let help = `
-    Welcome to LIRIBot\n
-    I'm a simple bot but powerful. You can search for movies, songs or bands.\n
-    Use the following parameters to get what you are looking for:\n\n
+	let help = `
+	Welcome to LIRIBot\n
+	I'm a simple bot but powerful. You can search for movies, songs or bands.\n
+	Use the following parameters to get what you are looking for:\n\n
 
-    node index.js spotify-this-song <Song title goes here>\n
-    node index.js movie-this <Movie title goes here>\n
-    node index.js concert-this <Band name goes here>\n\n
+	node index spotify-this-song <Song title goes here>\n
+	node index movie-this <Movie title goes here>\n
+	node index concert-this <Band name goes here>\n\n
 
-    Have fun!
-    `;
-    console.log(help);
+	Have fun!
+	`;
+	console.log(help);
+}
+
+function buildMessage(successFail, message) {
+	const seperator = '=====================\n';
+	let ret = seperator;
+	if (successFail == 'success') {
+		ret += `SUCCESS
+		${message}
+		`;
+		ret += seperator;
+		ret = chalk.yellow(ret.replace(/\t/g, ''));
+	} else {
+		ret += `FAIL
+		${message}
+		`;
+		ret += seperator;
+		ret = chalk.red(ret.replace(/\t/g, ''));
+	}
+	return ret;
+}
+
+function logger(message) {
+	fs.appendFileSync('log.txt', message + '\n');
 }
